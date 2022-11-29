4fe2305c38c566873e7fb11df21d2d8494d94992
Rafael Santos <rafa_santos33@outlook.com>
2022-11-29 13:02:12 -0300

    Home styling

---
+++ src/components/ButtonIcon/index.tsx
@@ -0,0 +1,9 @@
+import './styles.css';
+
+export const ButtonIcon = () => {
+    return (
+        <button className='btn btn-primary btn-icon'>
+            <h6>INICIE AGORA A SUA BUSCA</h6>
+        </button>
+    );
+}
