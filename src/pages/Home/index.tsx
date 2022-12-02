d2cf9bc909ab8b81764dda17860e0b1a3cd43c85
Rafael Santos <rafa_santos33@outlook.com>
2022-12-01 23:16:00 -0300

    Custom ButtonIcon

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -15,9 +15,11 @@ export const Home = () => {
               mercado.
             </p>
           </div>
-          <Link to='/products'>
-            <ButtonIcon />
-          </Link>
+          <div>
+            <Link to='/products'>
+              <ButtonIcon />
+            </Link>
+          </div>
         </div>
         <div className="home-image-container">
           <MainImage />
